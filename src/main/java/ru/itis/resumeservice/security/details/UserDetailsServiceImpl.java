4b7c23497773fe28d16f8a7fbe0a2260a8eaf1e1
DaniilBogomolov <bogomolov.daniil.99@gmail.com>
2020-10-27 16:35:18 +0300

    add security

---
+++ src/main/java/ru/itis/resumeservice/security/details/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package ru.itis.resumeservice.security.details;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import ru.itis.resumeservice.models.Applicant;
+import ru.itis.resumeservice.repositories.ApplicantRepository;
+
+import java.util.Optional;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private ApplicantRepository applicantRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<Applicant> applicantCandidate = applicantRepository.findByEmail(email);
+        if (applicantCandidate.isPresent()) {
+            Applicant applicant = applicantCandidate.get();
+            return new UserDetailsImpl(applicant);
+        }
+        throw new UsernameNotFoundException("User not found!");
+    }
+}
